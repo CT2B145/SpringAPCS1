4991fae26259e16829fb3feca39fa199317dcc5d
CT2B145 <mjpon45@gmail.com>
2017-04-16 22:25:06 -0700

    [BF] I am trying to cast an object array to a String array to make it easy to process. However, I get an exception of ava.lang.ArrayStoreException. which is really annoying.... Not sure why?? I can make an object array into a String array, but indiivdual cells in the array ARE STILL OBJECTS??

--- TextExcelStarterProject/TextExcel/src/textExcel/FormulaCell.java
+++ TextExcelStarterProject/TextExcel/src/textExcel/FormulaCell.java
@@ -1,44 +1,55 @@
 package textExcel;
 
+import java.util.Arrays;
+
 public class FormulaCell extends RealCell{
 	private String formanswer;
-	private String[] fractionEquationHolder;
+	//private String cellText = "";
+	private Cell[][] sprdsheet;	//saves array from spreadsheet to access information
+	private String[][] stringArray;// converts to string to save the hassle
 	// Constructor for the FormulaCell class
-	public FormulaCell (String enteredForm){
-		super(enteredForm);
-		this.formanswer = enteredForm;
+//	public FormulaCell (String enteredForm){
+//		super(enteredForm);
+//		this.formanswer = enteredForm;
+//	}
+	public FormulaCell (String enteredForm, Cell[][] emptycell){
+		super(enteredForm);							//constructor to fill super's String field (accesses by fullCellText)
+		this.formanswer = enteredForm;		
+		sprdsheet = emptycell;
+// The question that I cannot figure out, the object array changes to string, but the interior does not... WHY???
+		//stringArray = Arrays.copyOf(emptycell, emptycell.length, String[][].class);
+		//stringArray = emptycell.toArray(new String[][]);
 	}
-
-	@Override
-	public String abbreviatedCellText() {
+		@Override
+	public String abbreviatedCellText() { // processes the infomation here
 		
-		
-		if(this.formanswer.indexOf("( SUM")>= 0){ //for SUM
+		if(this.formanswer.indexOf("( SUM")>= 0){
+			// in theory its supposed to handle the sum of cells listed here
 			return"";
-		} else if(this.formanswer.indexOf("(")>= 0){ //for adding regular numbers
-
-			String removepart = this.formanswer.substring(2,this.formanswer.length()-2);
+		}else if((this.formanswer.indexOf("( AVG")>= 0)){
+			// in theory its supposed to handle the sum of cells listed here
+			return "";
+		} else if(this.formanswer.indexOf("(")>= 0){ //handles anything else that is not SUM or AVG
+		String removepart = this.formanswer.substring(2,this.formanswer.length()-2);
 		 String[] fracEquationHolder = removepart.split(" ",removepart.length()); // holds the fractions and operator
 	        String operators = "";
 	        double operand;
 	        double operand2;
-	        boolean hasUppercase = !fractionEquationHolder[0].substring(1).equals(fractionEquationHolder[0].toLowerCase());
-	        // object creation to reference sprdlocation to particular cell
-	       // SpreadsheetLocation location1 = new SpreadsheetLocation(fractionEquationHolder[0]);
-	      //  Spreadsheet getCell = new Spreadsheet();
-	        //if(fractionEquationHolder[0].matches(".*[a-zA-Z].*")){
-//	        if(hasUppercase){
-//	        fractionEquationHolder[0] = getCell.getCell(location1).fullCellText();
-//	        }
 	        double answer = Double.parseDouble(fracEquationHolder[0]);
-//	        // loop to do given math
+
+
 	        for(int i =0; i <= (fracEquationHolder.length-2); i+=2){
-//	        	hasUppercase = !fractionEquationHolder[i].substring(1).equals(fractionEquationHolder[i].toLowerCase());
-//		        //if(fractionEquationHolder[i+2].matches(".*[a-zA-Z]+.*")){
-//	        	if(hasUppercase){
-//			        fractionEquationHolder[i+2] = getCell.getCell(location1).fullCellText();
-//			        }
-	            operators = fracEquationHolder[i+1];
+	        	//This below in theory was supposed to handle "A1", etc. For some reason got an Arrays casting error?
+	    //================================================================================
+//				char charCell = cellText.charAt(i);	
+////			        Spreadsheet getCell1 = new Spreadsheet();
+//	        	if ((charCell >= 'a'&& charCell <= 'z') || (charCell >= 'A'&& charCell <= 'Z')){
+//	        		 fracEquationHolder[i] = getCell(fracEquationHolder[i]);
+//	        	}
+	        	
+	        	//Goal: to make it less lines as possible. But completely backfired.
+//====================================================================================================
+	        	operators = fracEquationHolder[i+1];
 	            operand = Double.parseDouble(fracEquationHolder[i]);
 	            operand2 = Double.parseDouble(fracEquationHolder[i+2]);
 	            if(operators.indexOf("+")>=0){
@@ -52,6 +63,7 @@ public class FormulaCell extends RealCell{
 	            }else
 	                return(answer+"      "); 
 	        }
+	        // handles the spacing
 	        String answer1 = answer +"";
 	        int cellValueLength = answer1.length();
 			if (cellValueLength < 10){
@@ -79,4 +91,29 @@ public class FormulaCell extends RealCell{
 		
 	}
 	
+//==============================================================================================
+	//For casting to the string array
+	public String getCell(String loc) {
+		int row = getRow(loc);
+		int col = getCol(loc);
+		return stringArray[row][col];
+	}
+    public int getRow(String test) {	//gets the row from the first letter in command
+    	
+    	int row = Integer.parseInt(test.substring(1))-1;
+        return row;
+    }
+
+    public int getCol(String col) // retrieves the column from the number value in command
+    {  	int j = 0;
+    	int colPosition = 0;
+    	if(col.charAt(0)>=0){
+    		String row = col.toUpperCase();
+    		char colfinal = row.charAt(0);
+    		j  = (int)colfinal;  	
+    		colPosition = j - 65; // since the ascii value of A = 65
+    	}
+        return colPosition;
+    }
 }
+	
