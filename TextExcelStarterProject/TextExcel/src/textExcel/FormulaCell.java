d558b8925032d34dbb675c46d967acf56412a19c
CT2B145 <mjpon45@gmail.com>
2017-05-08 22:04:55 -0700

    [BF] Still stuck. WHY CANT I CAST EMPTYCELL TO REALCELL, IT WORKS ON OTHER'S CODEEEEE. THIS IS #RIGGED!!!!

--- TextExcelStarterProject/TextExcel/src/textExcel/FormulaCell.java
+++ TextExcelStarterProject/TextExcel/src/textExcel/FormulaCell.java
@@ -5,14 +5,14 @@ public class FormulaCell extends RealCell{
 	//private String[] fractionEquationHolder;
 	private Spreadsheet sprdsheet;	//saves array from spreadsheet to access information from Spreadsheet
 	private Cell[][] breadsheet;
-	double answer;
+
 	// Constructor for the FormulaCell class
-	public FormulaCell (String enteredForm, Spreadsheet emptycell, Cell[][] emptycell2){
+	public FormulaCell (String enteredForm, Spreadsheet emptycell, Cell[][] breadsheet){
 		// TODO divided between Cell [][] or Spreadsheet object. Everytime I try Cell[][] objects, getDoubleValue method fails to exist.
 		super(enteredForm);							//constructor to fill super's String field (accesses by fullCellText)
 		formanswer = enteredForm;		
 		sprdsheet = emptycell;
-		breadsheet = emptycell2;
+		this.breadsheet = breadsheet;
 	}
 
 	public double sum(String total) { // adds the cells in the when SUM is requested
@@ -54,53 +54,49 @@ public class FormulaCell extends RealCell{
 	//}
 		return avgTotal/next;	
 	}
+//	public double calculate (String operator, double operand, double operand2){
+////	            operand = Double.parseDouble(opera);
+////	            operand2 = Double.parseDouble(b);
+//	            if(operator.indexOf("+")>=0){
+//	                answer += operand2;
+//	            }else if(operator.indexOf("-")>=0){
+//	                answer -= operand2;
+//	            }else if(operator.indexOf("*")>=0){
+//	                answer *=  operand2;
+//	            }else if(operator.indexOf("/")>=0){
+//	                answer /=   operand2;
+//	            }else{
+//	                return answer;
+//			}	                return answer;
+//
+//	}
 	@Override
 	public double getDoubleValue() {
 		String removepart = formanswer.substring(2,formanswer.length()-2);
 		 String[] fracEquationHolder = removepart.split(" ",removepart.length()); // holds the fractions and operator
 		  double operand;
 	      double operand2;
+	      double answer;
 		if (removepart.toUpperCase().indexOf("SUM")>=0){ //filters out SUM
-//			String removepart1 = this.formanswer.substring(2,this.formanswer.length()-2);
-//			return sum(removepart)+"";
 			return sum(fracEquationHolder[1]);
 		}else if (removepart.toUpperCase().indexOf("AVG")>=0) { //filters out AVG
-//			String removepart1 = this.formanswer.substring(2,this.formanswer.length()-2);
-//			return sum(removepart)+"";
-			return avg(fracEquationHolder[1]);
-		}else if (fracEquationHolder[0].substring(0,1).matches("[a-zA-Z]+")) { // um, using the famous Stackoverflow method
-			// handles anything that has letter in one request
-			SpreadsheetLocation gridLoc = new SpreadsheetLocation(fracEquationHolder[0]);
-			answer = ((RealCell) breadsheet[gridLoc.getRow()][gridLoc.getCol()]).getDoubleValue();
+			return sum(fracEquationHolder[1]);
 		}else{
-			answer = Double.parseDouble(fracEquationHolder[0]);
-		}
-		for (int i = 2; i < fracEquationHolder.length; i+=2) {
-			if (fracEquationHolder[i].length() < 4 && fracEquationHolder[i].substring(0,1).matches("[a-zA-Z]+")) {
-				// TODO Broken, not sure why. 
-				SpreadsheetLocation gridLoc = new SpreadsheetLocation(fracEquationHolder[i].toUpperCase());
-				//String holster = "";
-				double holster = ((RealCell) sprdsheet.getCell(gridLoc)).getDoubleValue();
-				if (fracEquationHolder[i-1].equals("+")) {
-					answer += ((RealCell) sprdsheet.getCell(gridLoc)).getDoubleValue();
-				}else if(fracEquationHolder[i-1].equals("-")) {
-					answer -= ((RealCell) sprdsheet.getCell(gridLoc)).getDoubleValue();
-				}else if(fracEquationHolder[i-1].equals("/")) {
-					answer /= ((RealCell) sprdsheet.getCell(gridLoc)).getDoubleValue();
-				}else if(fracEquationHolder[i-1].equals("*")) {
-					answer *= ((RealCell) sprdsheet.getCell(gridLoc)).getDoubleValue();
-				}else{
-					throw new IllegalArgumentException("Error, operator does not exist" );
+			for (int j = 0; j < fracEquationHolder.length; j+=2) {
+				if (fracEquationHolder[j].toUpperCase().charAt(0) >= 65) {
+					fracEquationHolder[j] = getCellValue(fracEquationHolder[j]) + "";
 				}
-			}else{ // TODO Checkpoint 1B is stated here. This else statement works perfectly fine. 
-				if (fracEquationHolder[i-1].equals("+")) {
-					answer += Double.parseDouble(fracEquationHolder[i]);
-				}else if(fracEquationHolder[i-1].equals("-")) {
-					answer -= Double.parseDouble(fracEquationHolder[i]);
-				}else if(fracEquationHolder[i-1].equals("/")) {
-					answer /= Double.parseDouble(fracEquationHolder[i]);
-				}else if (fracEquationHolder[i-1].equals("*")) {
-					answer *= Double.parseDouble(fracEquationHolder[i]);
+			} 
+			answer = Double.parseDouble(fracEquationHolder[0]);
+			for (int i = 1; i < fracEquationHolder.length; i+=2) {
+				if (fracEquationHolder[i].equals("+")) {
+					answer += Double.parseDouble(fracEquationHolder[i+1]);
+				}else if(fracEquationHolder[i].equals("-")) {
+					answer -= Double.parseDouble(fracEquationHolder[i+1]);
+				}else if(fracEquationHolder[i].equals("/")) {
+					answer /= Double.parseDouble(fracEquationHolder[i+1]);
+				}else if (fracEquationHolder[i].equals("*")) {
+					answer *= Double.parseDouble(fracEquationHolder[i+1]);
 				}else {
 					throw new IllegalArgumentException("ERro, operator does not exist");
 				}
@@ -108,7 +104,11 @@ public class FormulaCell extends RealCell{
 		}// for the needed spaces
 		return answer;
 	}
-	
+	public double getCellValue(String cellPlace) {
+		SpreadsheetLocation gridLoc = new SpreadsheetLocation(cellPlace);
+		double cellvalue =  ((RealCell) breadsheet[gridLoc.getRow()][gridLoc.getCol()]).getDoubleValue();
+		return cellvalue;
+	}
 	
 	
 	@Override
